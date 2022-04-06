2280aa021360518019405f97841de4d0a61642d7
aaron <achew012@e.ntu.edu.sg>
2022-04-06 09:55:33 +0800

    changed config

--- src/model/model.py
+++ src/model/model.py
@@ -44,35 +44,35 @@ class Transformer_CRF(nn.Module):
         super().__init__()
         self.num_labels = num_labels
         self.start_label_id = start_label_id
-        self.transitions = nn.Parameter(torch.randn(
-            self.num_labels, self.num_labels), requires_grad=True)
-        self.log_alpha = nn.Parameter(
-            torch.zeros(1, 1, 1), requires_grad=False)
+        self.transitions = nn.Parameter(
+            torch.randn(self.num_labels, self.num_labels), requires_grad=True
+        )
+        self.log_alpha = nn.Parameter(torch.zeros(1, 1, 1), requires_grad=False)
         self.score = nn.Parameter(torch.zeros(1, 1), requires_grad=False)
-        self.log_delta = nn.Parameter(
-            torch.zeros(1, 1, 1), requires_grad=False)
+        self.log_delta = nn.Parameter(torch.zeros(1, 1, 1), requires_grad=False)
         self.psi = nn.Parameter(torch.zeros(1, 1, 1), requires_grad=False)
-        self.path = nn.Parameter(torch.zeros(
-            1, 1, dtype=torch.long), requires_grad=False)
+        self.path = nn.Parameter(
+            torch.zeros(1, 1, dtype=torch.long), requires_grad=False
+        )
 
     @staticmethod
     def log_sum_exp_batch(log_Tensor, axis=-1):
         # shape (batch_size,n,m)
-        sum_score = torch.exp(log_Tensor - torch.max(log_Tensor, axis)
-                              [0].view(log_Tensor.shape[0], -1, 1)).sum(axis)
+        sum_score = torch.exp(
+            log_Tensor - torch.max(log_Tensor, axis)[0].view(log_Tensor.shape[0], -1, 1)
+        ).sum(axis)
         return torch.max(log_Tensor, axis)[0] + torch.log(sum_score)
 
     def reset_layers(self):
-        self.log_alpha = self.log_alpha.fill_(0.)
-        self.score = self.score.fill_(0.)
-        self.log_delta = self.log_delta.fill_(0.)
-        self.psi = self.psi.fill_(0.)
+        self.log_alpha = self.log_alpha.fill_(0.0)
+        self.score = self.score.fill_(0.0)
+        self.log_delta = self.log_delta.fill_(0.0)
+        self.psi = self.psi.fill_(0.0)
         self.path = self.path.fill_(0)
 
     def forward(self, feats, label_ids):
         forward_score = self._forward_alg(feats)
-        max_logLL_allz_allx, path, gold_score = self._crf_decode(
-            feats, label_ids)
+        max_logLL_allz_allx, path, gold_score = self._crf_decode(feats, label_ids)
         loss = torch.mean(forward_score - gold_score)
         self.reset_layers()
         return path, max_logLL_allz_allx, loss
@@ -82,12 +82,17 @@ class Transformer_CRF(nn.Module):
         # feats -> (batch size, num_labels)
         seq_size = feats.shape[1]
         batch_size = feats.shape[0]
-        log_alpha = self.log_alpha.expand(
-            batch_size, 1, self.num_labels).clone().fill_(-10000.)
+        log_alpha = (
+            self.log_alpha.expand(batch_size, 1, self.num_labels)
+            .clone()
+            .fill_(-10000.0)
+        )
         log_alpha[:, 0, self.start_label_id] = 0
         for t in range(1, seq_size):
-            log_alpha = (self.log_sum_exp_batch(self.transitions +
-                         log_alpha, axis=-1) + feats[:, t]).unsqueeze(1)
+            log_alpha = (
+                self.log_sum_exp_batch(self.transitions + log_alpha, axis=-1)
+                + feats[:, t]
+            ).unsqueeze(1)
         return self.log_sum_exp_batch(log_alpha)
 
     def _crf_decode(self, feats, label_ids):
@@ -95,20 +100,25 @@ class Transformer_CRF(nn.Module):
         batch_size = feats.shape[0]
 
         batch_transitions = self.transitions.expand(
-            batch_size, self.num_labels, self.num_labels)
+            batch_size, self.num_labels, self.num_labels
+        )
         batch_transitions = batch_transitions.flatten(1)
         score = self.score.expand(batch_size, 1)
 
-        log_delta = self.log_delta.expand(
-            batch_size, 1, self.num_labels).clone().fill_(-10000.)
+        log_delta = (
+            self.log_delta.expand(batch_size, 1, self.num_labels)
+            .clone()
+            .fill_(-10000.0)
+        )
         log_delta[:, 0, self.start_label_id] = 0
         psi = self.psi.expand(batch_size, seq_size, self.num_labels).clone()
 
         for t in range(1, seq_size):
             batch_trans_score = batch_transitions.gather(
-                -1, (label_ids[:, t] * self.num_labels + label_ids[:, t-1]).view(-1, 1))
-            temp_score = feats[:, t].gather(-1,
-                                            label_ids[:, t].view(-1, 1)).view(-1, 1)
+                -1,
+                (label_ids[:, t] * self.num_labels + label_ids[:, t - 1]).view(-1, 1),
+            )
+            temp_score = feats[:, t].gather(-1, label_ids[:, t].view(-1, 1)).view(-1, 1)
             score = score + batch_trans_score + temp_score
 
             log_delta, psi[:, t] = torch.max(self.transitions + log_delta, -1)
@@ -117,9 +127,8 @@ class Transformer_CRF(nn.Module):
         # trace back
         path = self.path.expand(batch_size, seq_size).clone()
         max_logLL_allz_allx, path[:, -1] = torch.max(log_delta.squeeze(), -1)
-        for t in range(seq_size-2, -1, -1):
-            path[:, t] = psi[:, t +
-                             1].gather(-1, path[:, t+1].view(-1, 1)).squeeze()
+        for t in range(seq_size - 2, -1, -1):
+            path[:, t] = psi[:, t + 1].gather(-1, path[:, t + 1].view(-1, 1)).squeeze()
 
         return max_logLL_allz_allx, path, score
 
@@ -144,20 +153,22 @@ class NERLongformer(pl.LightningModule):
 
         print("CUDA available: ", torch.cuda.is_available())
 
-        self.config = AutoConfig.from_pretrained(
-            self.cfg.model_name)
+        self.config = AutoConfig.from_pretrained(self.cfg.model_name)
         self.config.num_labels = len(self.cfg.role_map)
         self.config.attention_window = self.cfg.attention_window
         self.tokenizer = AutoTokenizer.from_pretrained(
             self.cfg.model_name, use_fast=True
         )
         self.model = AutoModelForTokenClassification.from_pretrained(
-            self.cfg.model_name, config=self.config)
+            self.cfg.model_name, config=self.config
+        )
 
         # Load tokenizer
         self.softmax = nn.Softmax(dim=-1)
         self.crf_layer = Transformer_CRF(
-            num_labels=self.config.num_labels, start_label_id=self.tokenizer.cls_token_id)
+            num_labels=self.config.num_labels,
+            start_label_id=self.tokenizer.cls_token_id,
+        )
 
         if cfg.grad_ckpt:
             self.model.longformer.gradient_checkpointing_enable()
@@ -167,7 +178,8 @@ class NERLongformer(pl.LightningModule):
 
         # Local attention everywhere - no global attention
         global_attention_mask = torch.zeros(
-            input_ids.shape, dtype=torch.long, device=input_ids.device)
+            input_ids.shape, dtype=torch.long, device=input_ids.device
+        )
 
         # Gradient Accumulation caveat 1:
         # For gradient accumulation to work, all model parameters should contribute
@@ -201,18 +213,18 @@ class NERLongformer(pl.LightningModule):
     def training_step(self, batch, batch_nb):
         """Call the forward pass then return loss"""
         outputs = self.forward(**batch)
-        return {'loss': outputs[0]}
+        return {"loss": outputs[0]}
 
     def _evaluation_step(self, split, batch, batch_nb):
         """Validaton or Testing - predict output, compare it with gold, compute rouge1, 2, L, and log result"""
         outputs = self.forward(**batch)
-        return {'loss': outputs[0], "logits": outputs[1]}
+        return {"loss": outputs[0], "logits": outputs[1]}
 
     def validation_step(self, batch, batch_nb):
-        outputs = self._evaluation_step('val', batch, batch_nb)
+        outputs = self._evaluation_step("val", batch, batch_nb)
         preds = outputs["logits"]
         labels = batch["labels"]
-        return {'val_loss': outputs["loss"], "preds": preds, "labels": labels}
+        return {"val_loss": outputs["loss"], "preds": preds, "labels": labels}
 
     def validation_epoch_end(self, outputs):
 
@@ -232,15 +244,23 @@ class NERLongformer(pl.LightningModule):
         val_logits = torch.cat(val_logits, 0)
         val_labels = torch.cat(val_labels, 0)
 
-        print(classification_report(val_labels.view(-1).cpu().detach(),
-              val_logits.view(-1).cpu().detach()))
+        print(
+            classification_report(
+                val_labels.view(-1).cpu().detach(), val_logits.view(-1).cpu().detach()
+            )
+        )
         self.log("val_loss", val_loss)
 
     def test_step(self, batch, batch_nb):
-        outputs = self._evaluation_step('val', batch, batch_nb)
+        outputs = self._evaluation_step("val", batch, batch_nb)
         preds = outputs["logits"]
         labels = batch["labels"]
-        return {'test_loss': outputs["loss"], "preds": preds, "labels": labels, "tokens": batch["input_ids"]}
+        return {
+            "test_loss": outputs["loss"],
+            "preds": preds,
+            "labels": labels,
+            "tokens": batch["input_ids"],
+        }
 
     def test_epoch_end(self, outputs):
         test_loss = []
@@ -263,11 +283,13 @@ class NERLongformer(pl.LightningModule):
         test_labels = torch.cat(test_labels, 0)
         test_tokens = torch.cat(test_tokens, 0)
 
-        tokens = [self.tokenizer.convert_ids_to_tokens(
-            token) for token in test_tokens]
+        tokens = [self.tokenizer.convert_ids_to_tokens(token) for token in test_tokens]
 
-        print(classification_report(test_labels.view(-1).cpu().detach(),
-              test_logits.view(-1).cpu().detach(), target_names=[key for key in self.cfg.role_map.keys()]))
+        print(
+            classification_report(
+                test_labels.view(-1).cpu().detach(), test_logits.view(-1).cpu().detach()
+            )
+        )
 
     def configure_optimizers(self):
         """Configure the optimizer and the learning rate scheduler"""
@@ -275,8 +297,10 @@ class NERLongformer(pl.LightningModule):
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
-                "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', verbose=True),
+                "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
+                    optimizer, "min", verbose=True
+                ),
                 "monitor": "val_loss",
-                "frequency": 1
-            }
+                "frequency": 1,
+            },
         }
